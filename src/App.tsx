ac2085372d08596edf9bcce1f6a42ae1c8a8f8d7
Paulo Nobrega <paulo.nobrega@mapesolutions.com>
2022-12-09 20:25:44 -0300

    1. Configurando modal de criação.mp4

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,35 @@
 import { Dashboard } from "./components/Dashboard/indes";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
+import Modal from "react-modal";
+import { useState } from "react";
+
+Modal.setAppElement("#root");
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenTransactionModal} />
       <Dashboard />
+
+      <Modal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </Modal>
+
       <GlobalStyle />
     </>
   );
